bea1bf304403b49fcd84535ab52c5d457a1b6a66
MohsenPsh71 <78726489+MohsenPsh71@users.noreply.github.com>
2021-02-27 10:10:43 +0330

    Change DateTime Format

--- Java Script/javalibrary/javatest2.js
+++ Java Script/javalibrary/javatest2.js
@@ -28,10 +28,43 @@ fetch('https://reqres.in/api/users').then(Response => {
 }).then(data => {
     console.log(data)
     var groops = "";
-    for (var i = 0; i < 6 ;i++) {
+    for (var i = 0; i < 6; i++) {
         var modify = document.getElementById('update2');
         console.log(data.data[i].email);
         groops += data.data[i].email + '<br/>';
     }
     modify.innerHTML = groops;
 });
+
+//"24/02/2021"
+//to
+//"2021/02/24"
+
+function GetFormattedDate(X) {
+    debugger;
+    var d = new Date();
+    console.log(d);
+    //var a = d.();
+    //console.log(a);
+    //var ca = d.split('/');
+    var month = d.getMonth();
+    if (month < 10) month = "0" + month;
+    var day = d.getDate();
+    if (day < 10) day = "0" + day;
+    var year = d.getFullYear();
+    if (X === 'mmm,dd,yyyy') {
+        return document.getElementById('datatimechenge').innerHTML = month + "/" + day + "/" + year;
+    }
+    if (X === 'yyyy,dd,mmm') {
+        return document.getElementById('datatimechenge').innerHTML = year + "/" + day + "/" + month;
+    }
+    if (X === 'yyyy,mmm,dd') {
+        return document.getElementById('datatimechenge').innerHTML = year + "/" + month + "/" + day;
+    }
+    if (X === 'dd,mmm,yyyy') {
+        return document.getElementById('datatimechenge').innerHTML = month + "/" + day + "/" + year;
+    }
+}
+
+GetFormattedDate('yyyy,dd,mmm');
+
